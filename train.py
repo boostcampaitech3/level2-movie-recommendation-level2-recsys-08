7715281deb49b487b7009ff4397755a7d3be68d6
qwon135 <qwon135@gmail.com>
2022-03-21 03:08:23 +0000

    [add] - [train.py 예시?]

---
+++ train.py
@@ -0,0 +1,20 @@
+import argparse
+import glob
+import json
+import os
+import random
+import re
+
+
+def train(args):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
+    parser.add_argument('--epochs', type=int, default=300, help='number of epochs to train (default: 1)')
+
+    args = parser.parse_args()
+    print(args)
+
+    train(args)
